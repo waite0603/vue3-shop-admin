8c8f7a4f81bde79f6c1efb0ed85206e78a26e424
Waite Wang <waite0603@qq.com>
2023-02-12 00:07:10 +0800

    stylelint忽略规则(Warning)apply, WindiCSS引入@apply 简化代码

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,13 @@
 </script>
 
 <template>
-  <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
+  <button class='btn'>
+    按钮
+  </button>
 </template>
 
-<style scoped>
-
+<style>
+  .btn{
+    @apply bg-purple-500 text-indigo-50 px-4 py-2 rounded-full transition-all duration-500 hover:( bg-purple-900) focus:(ring-8 ring-purple-900);
+  }
 </style>
