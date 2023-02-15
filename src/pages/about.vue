691e000a87f7910abde59a7dcb47aee846820b5b
Waite Wang <waite0603@qq.com>
2023-02-16 01:29:49 +0800

    setup 语法糖 以及 @apply 封装

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,67 @@
 <template>
-    <div>
-        about
-    </div>
+    <ul>
+        <li>{{ myref0 }}</li>
+        <li>{{ myref1 }}</li>
+        <li>{{ myref2 }}</li>
+        <li>{{ myref3 }}</li>
+        <li>{{ myrea0 }}</li>
+        <li>{{ myrea1 }}</li>
+        <li>{{ myrea2.age }}</li>
+        <li>{{ myrea3.user.name }}</li>
+        <li>{{ myrea4.one.three.age.four }}</li>
+    </ul>
+    <el-button @click="change">change</el-button>
 </template>
+
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+    setup() {
+        let myref0 = ref(1);
+        let myref1 = ref(2);
+        let myref2 = ref(true);
+        let myref3 = ref('myref3')
+        let myrea0 = reactive(0);
+        let myrea1 = reactive('myrea1');
+        let myrea2 = reactive({ age: 3 })
+        let myrea3 = reactive({
+            user: {
+                name: "zs",
+            }
+        })
+        let myrea4 = reactive({
+            top: 'top', one: {
+                two: "two",
+                three: {
+                    name: "LS",
+                    age: {
+                        four: "four"
+                    }
+                },
+            }
+        })
+        function change() {
+            myref0 = 2;
+            myref1.value++;
+            myref2.value = false;
+            myref3.value = 'MyRef333'
+            myrea0++;
+            myrea1 = 'myrea1111';
+            myrea2.age++;
+            myrea3.user.name = "ww";
+            myrea4.one.three.age.four = "five";
+            console.log(myref0);
+            console.log(myrea0);
+            console.log(myrea1);
+        }
+        return { myref0, myrea0, myref1, myrea1, myref2, myrea2, myref3, myrea3, myrea4, change }
+    }
+}
+/*
+1.ref包裹简单类型，可以响应数据变化。
+2.reactive包裹简单类型，不可以响应数据变化。
+3.在更改ref包裹的值时，需要加 .value 来触发响应。
+4.在页面中使用，{{ }}进行文本插值时，即使使用ref包裹也不需要添加 .value 属性，得益于在检测到 __v_isRef:true 时，vue会帮我们添加。
+*/
+</script>
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,25 +1,24 @@
 <template>
-  <el-row class="min-h-screen">
-    <el-col :lg="16" :md="12" class="bg-blue-600 flex items-center justify-center flex-col">
+  <el-row class="login-container">
+    <el-col :lg="16" :md="12" class="left">
       <div>
-        <h1 class="text-6xl text-white font-bold pr-5 pl-5">Welcome to</h1>
-        <h3 class="text-2xl text-white font-bold mt-4 pr-5 pl-5">
+        <h1 class="login-container-h1">Welcome to</h1>
+        <h3 class="login-container-h3">
           基于Vue3 + ElementPlus + Vite实战开发商城后台管理系统
         </h3>
-        <div class="mt-7 text-right pr-5">
-          <el-link type="primary" class="text-white" @click="JumpToGithub">
-            <i class="iconfont icon-icon_github mr-1"></i>
-            Github
+        <div class="login-container-github-text">
+          <el-link type="primary" @click="JumpToGithub">
+            <span>
+              <i class="iconfont icon-icon_github mr-1"></i>Github
+            </span>
           </el-link>
         </div>
       </div>
     </el-col>
-    <el-col :lg="8" :md="12" class="bg-light-50 flex items-center justify-center flex-col">
-      <div class="text-2xl font-bold mb-2">欢迎回来</div>
-      <div class="flex items-center justify-center mb-4 text-gray-500 space-x-2">
-        <span class="h-[1px] w-16 bg-gray-200"></span>
-        <span>账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12" class="right">
+      <div class="welcome-title">欢迎回来</div>
+      <div class="welcome-text">
+        <span></span>账号密码登录<span></span>
       </div>
       <el-form :model="form" class="w-2/4">
         <el-form-item>
@@ -68,3 +67,37 @@ const JumpToGithub = () => {
 };
 
 </script>
+
+<style scoped>
+.login-container{
+    @apply min-h-screen;
+}
+.login-container .left{
+  @apply bg-blue-600 flex items-center justify-center flex-col;
+}
+.login-container .right{
+  @apply bg-light-50 flex items-center justify-center flex-col
+}
+.left>div>.login-container-h1{
+    @apply font-bold text-6xl text-light-50 m-4;
+}
+.left>div>.login-container-h3{
+    @apply font-bold text-2xl text-light-50 m-4;
+}
+.left>div>.login-container-github-text{
+  @apply mt-7 text-right pr-5;
+}
+.left>div>.login-container-github-text span{
+  @apply text-white;
+}
+
+.right .welcome-title{
+    @apply text-2xl font-bold mb-2 text-black;
+}
+.right .welcome-text{
+    @apply flex items-center justify-center text-gray-400 mb-4;
+}
+.right .welcome-text span{
+    @apply h-[1px] w-16 bg-gray-200;
+}
+</style>
